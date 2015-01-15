fe3a17b5b60483f9459e794b3c064ced015241c5
Oliver <o.lucas@live.com>
2015-01-15 13:54:27 +0000

    small mistake modified

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -19,7 +19,7 @@ feature "User signs up" do
     visit '/users/new'
     expect(page.status_code).to eq(200)
     fill_in :email, :with => email
-    fill_in :password. :with => password
+    fill_in :password, :with => password
     click_button "Sign up"
   end
 
