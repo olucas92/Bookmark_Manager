d00fc17e2cb816b3f30dbfb27e557e7033f73cb7
Oliver <o.lucas@live.com>
2015-01-28 19:13:53 +0000

    test for signing out written

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -68,3 +68,18 @@ feature "User signs in" do
     click_button 'Sign in'
   end
 end
+
+feature "User signs out" do
+  before(:each) do
+    User.create(:email => 'test@test.com'
+                :password => 'test'
+                :password_confirmation => 'test')
+  end
+
+  scenario "while being signed in" do
+    sign_in('test@test.com', 'test')
+    click_button 'Sign out'
+    expect(page).to have_content("Goodbye!") #Where does this message go?
+    expect(page).to_not have_content("Welcome, test@test.com")
+  end
+end
